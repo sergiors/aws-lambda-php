9bf1e0ae50da36f74f4c54f3491e86430ee6f28f
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2021-02-26 12:05:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+```bash
+docker-compose up -d
+docker-compose down
+```
+
+`-d` Detached mode: Run containers in the background
+
+```bash
+docker volume ls
+docker volume rm VOLUME
+```
---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_host = getenv('DB_HOST');
+$db_name = getenv('DB_NAME');
+$db_user = getenv('DB_USER');
+$db_password = getenv('DB_PASSWORD');
+
+
+var_dump($db_host, $db_name, $db_user, $db_password);
